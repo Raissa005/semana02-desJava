c227c2c4ce78ec2111aaecf1891a2471a7b165c9
raissa_leite <raissa_leite@SN-358997.edu.fiesc.com.br>
2023-07-27 19:33:37 -0300

    proj NetBeans

--- src/views/PomodoroAdvence.java
+++ src/views/PomodoroAdvence.java
@@ -12,6 +12,10 @@ public class PomodoroAdvence extends javax.swing.JFrame {
         initComponents();
     }
 
+private final int FOCUS_TIME = 25;
+private final int FAST_BREAK = 5;
+private final int LONG_BREAK_TIME = 15;
+private final int ROUNDS = 4;
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -37,6 +41,8 @@ public class PomodoroAdvence extends javax.swing.JFrame {
         sliderRounds = new javax.swing.JSlider();
         sliderTempoFoco = new javax.swing.JSlider();
         labelBreakLongo = new javax.swing.JLabel();
+        buttonAplic = new javax.swing.JButton();
+        resetButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -110,36 +116,46 @@ public class PomodoroAdvence extends javax.swing.JFrame {
         jLabel1.setText("TEMPO DE FOCO");
 
         labelTempoFoco.setFont(new java.awt.Font("Dutch801 Rm BT", 0, 24)); // NOI18N
-        labelTempoFoco.setForeground(new java.awt.Color(0, 153, 102));
+        labelTempoFoco.setForeground(new java.awt.Color(0, 153, 153));
         labelTempoFoco.setText("25:00");
 
         jLabel14.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(0, 153, 102));
         jLabel14.setText("BREAK CURTO");
 
         jLabel18.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
+        jLabel18.setForeground(new java.awt.Color(0, 153, 102));
         jLabel18.setText("BREAK LONGO");
 
         jLabel22.setFont(new java.awt.Font("DialogInput", 1, 24)); // NOI18N
+        jLabel22.setForeground(new java.awt.Color(0, 153, 102));
         jLabel22.setText("ROUNDS");
 
         labelRounds.setFont(new java.awt.Font("Dutch801 Rm BT", 0, 24)); // NOI18N
+        labelRounds.setForeground(new java.awt.Color(0, 153, 153));
         labelRounds.setText("25:00");
 
         labelBreakCurto.setFont(new java.awt.Font("Dutch801 Rm BT", 0, 24)); // NOI18N
-        labelBreakCurto.setText("25:00");
+        labelBreakCurto.setForeground(new java.awt.Color(0, 153, 153));
+        labelBreakCurto.setText("5:00");
 
+        sliderBreakCurto.setForeground(new java.awt.Color(0, 153, 153));
         sliderBreakCurto.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 sliderBreakCurtoStateChanged(evt);
             }
         });
 
+        sliderBreakLongo.setForeground(new java.awt.Color(0, 153, 153));
+        sliderBreakLongo.setMaximum(30);
+        sliderBreakLongo.setMinimum(15);
         sliderBreakLongo.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 sliderBreakLongoStateChanged(evt);
             }
         });
 
+        sliderRounds.setForeground(new java.awt.Color(0, 153, 153));
         sliderRounds.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 sliderRoundsStateChanged(evt);
@@ -154,7 +170,22 @@ public class PomodoroAdvence extends javax.swing.JFrame {
         });
 
         labelBreakLongo.setFont(new java.awt.Font("Dutch801 Rm BT", 0, 24)); // NOI18N
-        labelBreakLongo.setText("25:00");
+        labelBreakLongo.setForeground(new java.awt.Color(0, 153, 153));
+        labelBreakLongo.setText("15:00");
+
+        buttonAplic.setText("Aplicar");
+        buttonAplic.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonAplicActionPerformed(evt);
+            }
+        });
+
+        resetButton.setText("Reset Default");
+        resetButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                resetButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout settingsLayout = new javax.swing.GroupLayout(settings);
         settings.setLayout(settingsLayout);
@@ -197,6 +228,12 @@ public class PomodoroAdvence extends javax.swing.JFrame {
                 .addGap(382, 382, 382)
                 .addComponent(labelRounds)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, settingsLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(resetButton)
+                .addGap(31, 31, 31)
+                .addComponent(buttonAplic, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
             .addGroup(settingsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(settingsLayout.createSequentialGroup()
                     .addGap(388, 388, 388)
@@ -233,12 +270,16 @@ public class PomodoroAdvence extends javax.swing.JFrame {
                 .addComponent(labelRounds)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(sliderRounds, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(194, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                .addGroup(settingsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(resetButton)
+                    .addComponent(buttonAplic))
+                .addGap(24, 24, 24))
             .addGroup(settingsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(settingsLayout.createSequentialGroup()
                     .addGap(310, 310, 310)
                     .addComponent(labelBreakLongo)
-                    .addContainerGap(332, Short.MAX_VALUE)))
+                    .addContainerGap(275, Short.MAX_VALUE)))
         );
 
         painelPrincip.add(settings, "settings");
@@ -285,7 +326,18 @@ public class PomodoroAdvence extends javax.swing.JFrame {
       sliderLabel(sliderRounds, labelRounds);
     }//GEN-LAST:event_sliderRoundsStateChanged
 
+    private void buttonAplicActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonAplicActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonAplicActionPerformed
+
+    private void resetButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetButtonActionPerformed
+        this.sliderTempoFoco.setValue(this.FOCUS_TIME);
+        this.sliderBreakCurto.setValue(this.LONG_BREAK_TIME);
+        this.sliderRounds.setValue(this.ROUNDS);
+    }//GEN-LAST:event_resetButtonActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonAplic;
     private javax.swing.JLabel fechaSettings;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel14;
@@ -297,6 +349,7 @@ public class PomodoroAdvence extends javax.swing.JFrame {
     private javax.swing.JLabel labelTempoFoco;
     private javax.swing.JPanel painelPrincip;
     private javax.swing.JLabel playAndPause;
+    private javax.swing.JButton resetButton;
     private javax.swing.JPanel settings;
     private javax.swing.JLabel settingsButton;
     private javax.swing.JSlider sliderBreakCurto;
